425c8f0a2b3078eed4c30faee66b92f465f21779
Nyat Ghirmazion <nyat.ghirmazion@chasacademy.se>
2024-11-22 12:09:55 +0100

    product component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import ProductComponent from './components/ProductComponent.vue';
+</script>
 
 <template>
   <h1>App.vue</h1>
+  <ProductComponent/>
 </template>
 
 <style scoped></style>
