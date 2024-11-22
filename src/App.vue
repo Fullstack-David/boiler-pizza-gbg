d9643eba78b59489eab763e3133bd2ee7d62ae8c
Emil <emil.winberg1@gmail.com>
2024-11-22 11:25:56 +0100

    Implement simple image switch on 'no-image'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+</script>
 
 <template>
   <h1>App.vue</h1>
+  <HelloWorld/>
 </template>
 
 <style scoped></style>
