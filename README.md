fcffda4453033c33cee30757872793c229c9c568
Jens Bengtsson <jb225je@student.lnu.se>
2024-11-22 11:57:33 +0100

    switch from axios to fetch

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # boiler-pizza-gbg
+1. npm i
 
-Skapa .env fil som innehåller:
+2. Skapa .env fil som innehåller:
 API_KEY_PIZZA_MENU=sp-B2mWxADrthdHqd22
+
+3. 
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+  import FetchPizzaMenu from './components/FetchPizzaMenu.vue';
+</script>
 
 <template>
   <h1>App.vue</h1>
+  <FetchPizzaMenu />
 </template>
 
 <style scoped></style>
